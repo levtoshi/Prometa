75c12238e90aa65492fd2ca47d66e2b2b5eb4a2d
Levtoshi <154081940+levtoshi@users.noreply.github.com>
2024-02-22 19:14:48 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,7 +13,6 @@ buttons = []
 def clear():
     for i in buttons:
         i.destroy()
-    ThemeController.change_theme.destroy()
     buttons.clear()
 
 def back():
@@ -24,15 +23,13 @@ def back():
     back_.place(x=10, y=10)
 
 def menu():
-    for i in range(1,12):
-        for i, (text, icon, command) in enumerate(zip(button_texts, button_icons, button_command), start=1):
-            button = CTkButton(win, text=text, font=button_font, fg_color=button_color,
-                               image=icon, compound=button_compound,
-                               width=button_g, height=button_g, hover_color=button_hover_color,
-                               corner_radius=button_radius, command=command)
-            button.place(x=23 + ((i - 1) % 4) * 197, y=90 + ((i - 1) // 4) * 200)
-            buttons.append(button)
-        ThemeController.createButton()
+    for i, (text, icon, command) in enumerate(zip(button_texts, button_icons, button_command), start=1):
+        button = CTkButton(win, text=text, font=button_font, fg_color=button_color,
+                            image=icon, compound=button_compound,
+                            width=button_g, height=button_g, hover_color=button_hover_color,
+                            corner_radius=button_radius, command=command)
+        button.place(x=23 + ((i - 1) % 4) * 197, y=90 + ((i - 1) // 4) * 200)
+        buttons.append(button)
     back_.destroy()
 
 def calendar():
